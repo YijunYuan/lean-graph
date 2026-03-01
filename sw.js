246417eef0ce8ba805dc042e890a208e4cdcee9c
Yijun Yuan <941201yuan@gmail.com>
2026-03-01 20:52:17 +0800

    添加服务工作者 sw.js 以支持注册和未来的缓存逻辑

---
+++ sw.js
@@ -0,0 +1,8 @@
+// Minimal service worker to satisfy registration and allow future cache logic.
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
